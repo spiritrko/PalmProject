c1c301ca6042dd27aad2f36cdebe10a09ae33e0f
Jirawat <spiritrko@gmail.com>
2022-06-21 12:03:46 +0700

    SaveProject

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -3,15 +3,27 @@ import 'package:palmpea/widgets/show_form.dart';
 import 'package:palmpea/widgets/show_image.dart';
 import 'package:palmpea/widgets/show_text.dart';
 
-class Authen extends StatelessWidget {
+class Authen extends StatefulWidget {
   const Authen({Key? key}) : super(key: key);
+
+  @override
+  State<Authen> createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  bool redEye = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints boxConstraints) {
-        return Center(
+        return 
+        GestureDetector(behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusScopeNode());
+          },
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -21,22 +33,30 @@ class Authen extends StatelessWidget {
               formPassword(boxConstraints)
             ],
           ),
-        );
+        ),
+ );
+
       }),
     );
   }
 
   Container formPassword(BoxConstraints boxConstraints) {
     return Container(
-              margin: const EdgeInsets.only(top: 16),
-              width: boxConstraints.maxWidth*0.6,
-              height: 40,
-              child: ShowForm(
-                hint: 'Password',
-                iconData: Icons.lock_outline,
-                changeFung: (String string) {},
-              ),
-            );
+      margin: const EdgeInsets.only(top: 16),
+      width: boxConstraints.maxWidth * 0.6,
+      height: 40,
+      child: ShowForm(
+        redEyeFunc: () {
+          setState(() {
+            redEye = !redEye;
+          });
+        },
+        obSecu: redEye,
+        hint: 'Password',
+        iconData: Icons.lock_outline,
+        changeFung: (String string) {},
+      ),
+    );
   }
 
   Container formUser(BoxConstraints boxConstraints) {
