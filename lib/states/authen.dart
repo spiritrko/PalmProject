087ea666f1e88df9c4d02065ceb260034dcbbdb0
Jirawat <spiritrko@gmail.com>
2022-06-21 11:24:59 +0700

    form success

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:palmpea/widgets/show_form.dart';
 import 'package:palmpea/widgets/show_image.dart';
 import 'package:palmpea/widgets/show_text.dart';
 
@@ -11,16 +12,47 @@ class Authen extends StatelessWidget {
       body: LayoutBuilder(
           builder: (BuildContext context, BoxConstraints boxConstraints) {
         return Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               newLogo(boxConstraints),
-              ShowTest(text: 'Login :'),
+              newTitle(),
+              formUser(boxConstraints),
+              formPassword(boxConstraints)
             ],
           ),
         );
       }),
     );
   }
+
+  Container formPassword(BoxConstraints boxConstraints) {
+    return Container(
+              margin: const EdgeInsets.only(top: 16),
+              width: boxConstraints.maxWidth*0.6,
+              height: 40,
+              child: ShowForm(
+                hint: 'Password',
+                iconData: Icons.lock_outline,
+                changeFung: (String string) {},
+              ),
+            );
+  }
+
+  Container formUser(BoxConstraints boxConstraints) {
+    return Container(
+      margin: const EdgeInsets.only(top: 16),
+      width: boxConstraints.maxWidth * 0.6,
+      height: 40,
+      child: ShowForm(
+        hint: 'User:',
+        iconData: Icons.person_outline,
+        changeFung: (String string) {},
+      ),
+    );
+  }
+
+  ShowTest newTitle() => ShowTest(text: 'Login :');
 
   SizedBox newLogo(BoxConstraints boxConstraints) {
     return SizedBox(
